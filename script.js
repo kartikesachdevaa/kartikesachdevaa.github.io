760a7347e5081b24afc95b336039577bcc6f38e2
Kartike Sachdeva <158793062+kartikesachdevaa@users.noreply.github.com>
2025-03-12 04:18:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal('.hero-content', {
+  delay: 200,
+  duration: 1000,
+  origin: 'top',
+  distance: '50px',
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.about', {
+  delay: 400,
+  duration: 1000,
+  origin: 'left',
+  distance: '100px',
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.portfolio', {
+  delay: 600,
+  duration: 1000,
+  origin: 'right',
+  distance: '100px',
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.contact', {
+  delay: 800,
+  duration: 1000,
+  origin: 'bottom',
+  distance: '100px',
+  easing: 'ease-in-out'
+});
